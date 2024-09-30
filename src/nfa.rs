9dacf825060c86a056d48cb032d135eeac1c994c
Anton Plusnin <anton@vipset.com>
2024-09-30 03:40:46 +0300

    compress epsilons

--- src/nfa.rs
+++ src/nfa.rs
@@ -5,25 +5,26 @@ use crate::automaton::Automaton;
 use crate::state::*;
 use std::fmt;
 
-pub struct NfaState<T: Traversable + Display> {
+#[derive(Clone)]
+pub struct NfaState<T: Traversable + Display + Clone> {
     pub index: i32,
     pub is_terminal: bool,
     transitions: Vec<(T, i32)>
 }
 
-impl<T: Traversable + Display> NfaState<T> {
+impl<T: Traversable + Display + Clone> NfaState<T> {
     pub fn new(index: i32, is_terminal: bool) -> Self {
         Self{index: index, is_terminal: is_terminal, transitions: Vec::new()}
     }
 }
 
-impl<T: Traversable + Display> NfaState<T> {
+impl<T: Traversable + Display + Clone> NfaState<T> {
     pub fn add_transition(&mut self, word: T, next_state: i32) {
         self.transitions.push((word, next_state));
     }
 }
 
-impl<T: Traversable + Display> State for NfaState<T> {
+impl<T: Traversable + Display + Clone> State for NfaState<T> {
     fn next(&self, str: &str) -> Vec<(i32, String)> {
         let mut result = Vec::new();
         for (word, state) in &self.transitions {
@@ -36,12 +37,13 @@ impl<T: Traversable + Display> State for NfaState<T> {
     }
 }
 
-pub struct Nfa<T: Traversable + Display> {
+#[derive(Clone)]
+pub struct Nfa<T: Traversable + Display + Clone> {
     starting_state: i32,
     states: BTreeMap<i32, NfaState<T>>
 }
 
-impl<T: Traversable + Display> Nfa<T> {
+impl<T: Traversable + Display + Clone> Nfa<T> {
     pub fn new(starting_state: i32) -> Self {
         Self{starting_state: starting_state, states: BTreeMap::new()}
     }
@@ -51,7 +53,7 @@ impl<T: Traversable + Display> Nfa<T> {
     }
 }
 
-impl<T: Traversable + Display> Automaton<'_> for Nfa<T> {
+impl<T: Traversable + Display + Clone> Automaton<'_> for Nfa<T> {
     fn accept<'a>(&self, str: &'a str) -> bool {
         let mut counter = (str.len() + 1) * (self.states.len() + 1) * (self.states.len() + 1);
         let mut queue: VecDeque<(i32, String)> = VecDeque::new();
@@ -70,18 +72,46 @@ impl<T: Traversable + Display> Automaton<'_> for Nfa<T> {
     }
 }
 
-impl<T: Traversable + Display> fmt::Display for Nfa<T> {
+impl Nfa<String> {
+    pub fn compress_eps(&self) -> Self {
+        let mut result = self.clone();
+        for (idx, _) in self.states.iter() {
+            let mut new_transitions = vec![];
+            let mut is_terminal = false;
+            let mut used = vec![false; self.states.len()];
+            let mut queue: VecDeque<i32> = VecDeque::new();
+            queue.push_back(*idx);
+            while !queue.is_empty() {
+                let cur = queue.pop_front().unwrap();
+                is_terminal |= result.states[&cur].is_terminal;
+                used[cur as usize] = true;
+                for (transition_word, next_state) in self.states[&cur].transitions.iter() {
+                    if transition_word.len() > 0 {
+                        new_transitions.push((transition_word.clone(), next_state.clone()));
+                    } else if !used[*next_state as usize] {
+                        queue.push_back(*next_state);
+                    }
+                }
+            }
+            result.states.get_mut(idx).unwrap().transitions = new_transitions;
+            result.states.get_mut(idx).unwrap().is_terminal = is_terminal;
+        }
+        result
+    }
+}
+
+impl<T: Traversable + Display + Clone> fmt::Display for Nfa<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         writeln!(f, "Starting state: {}", self.starting_state)?;
-        for (state, value) in self.states.iter() {
-            for (word, next_state) in value.transitions.iter() {
-                write!(f, "{state} -> {next_state} by \"{word}\"")?;
+        for (idx, state) in self.states.iter() {
+            for (word, next_state) in state.transitions.iter() {
+                writeln!(f, "{idx} -> {next_state} by \"{word}\"")?;
             }
         }
         writeln!(f, "Terminal states:")?;
-        for (state, value) in self.states.iter() {
-            if value.is_terminal {
-                writeln!(f, "{state}")?;
+        for (idx, state) in self.states.iter() {
+            if state.is_terminal {
+                writeln!(f, "{idx}")?;
             }
         }
         Ok(())
